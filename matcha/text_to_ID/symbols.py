88e7ba5e7b6a0968601a599d8dac7c85694c5375
pmdemars-1998 <demars.pm@gmail.com>
2025-12-28 14:27:10 +0100

    Update symbols.py
    ajout des caractères inconnus unk

--- matcha/text_to_ID/symbols.py
+++ matcha/text_to_ID/symbols.py
@@ -4,14 +4,15 @@ Defines the set of symbols used in text input to the model.
 The default is a set of ASCII characters that works well for English or text that has been run
 through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details.
 '''
-from matcha.text_to_ID import cmudict
+from .cmudict import valid_symbols
 
 _pad        = '_'
 _eos        = '~'
+_unk        = '<unk>'  # Ajout du symbole pour les mots inconnus
 _characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\'(),-.:;? '
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
-_arpabet = ['@' + s for s in cmudict.valid_symbols]
+_arpabet = ['@' + s for s in valid_symbols]
 
 # Export all symbols:
-symbols = [_pad, _eos] + list(_characters) + _arpabet
+symbols = [_pad, _eos, _unk] + list(_characters) + _arpabet
