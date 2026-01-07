6a37f5988b2c9dd23ec69eaf42826ea31487407b
pmdemars-1998 <demars.pm@gmail.com>
2026-01-07 16:31:30 +0100

    Update ljspeechDataset.py sans phonemisation

--- matcha/data_management/ljspeechDataset.py
+++ matcha/data_management/ljspeechDataset.py
@@ -1,20 +1,22 @@
 import os
 import torch
 from torch.utils.data import Dataset
-# On importe la nouvelle fonction text_to_sequence
-from matcha.text_to_ID.text_to_sequence import text_to_sequence
 from matcha.text_to_ID.cleaners import english_cleaners
+from matcha.text_to_ID.symbols import symbols
 from matcha.utils.audio_process import MelSpectrogram, load_and_process_audio
 
 class LJSpeechDataset(Dataset):
     def __init__(self, metadata_path):
+        
+        # On lit le fichier train.txt ou val.txt (format: chemin_wav|texte)
         with open(metadata_path, "r", encoding="utf-8") as f:
             self.metadata = [line.strip().split("|") for line in f.readlines()]
         
-        # Le dictionnaire symbol_to_id est maintenant géré dans text_to_sequence
-        
+        self.symbol_to_id = {s: i for i, s in enumerate(symbols)}
         self.mel_proc = MelSpectrogram(n_fft=1024, num_mels=80, sampling_rate=22050, 
                                        hop_size=256, win_size=1024, fmin=0, fmax=8000)
+        self.mel_mean = -5.517955  # Remplacez par votre valeur calculée
+        self.mel_std = 2.064464
 
     def __len__(self):
         return len(self.metadata)
@@ -22,16 +24,14 @@ class LJSpeechDataset(Dataset):
     def __getitem__(self, idx):
         wav_path, raw_text = self.metadata[idx]
 
-        # 1. Nettoyage basique (optionnel si phonemizer gère bien le brut)
+        # 1. Traitement du Texte
         clean_text = english_cleaners(raw_text)
-        
-        # 2. Phonémisation et Conversion en IDs
-        #  le texte devient des phonèmes puis des IDs
-        sequence = text_to_sequence(clean_text, ["english_cleaners"])
-        text_ids = torch.tensor(sequence, dtype=torch.long)
+        text_ids = torch.tensor([self.symbol_to_id[s] for s in clean_text if s in self.symbol_to_id], dtype=torch.long)
 
-        # 3. Traitement Audio
+        # 2. Traitement Audio (wav_path est déjà le chemin complet grâce à ljspeech.py)
         mel = load_and_process_audio(wav_path, self.mel_proc)
+
+        mel = (mel - self.mel_mean) / self.mel_std
 
         return {
             "x": text_ids, 
